23a736888211501cfd96841e0495e1d8152d04d7
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-03-09 08:01:13 +0000

    Edited settings and Error_Tracker

--- server_code/Error_Tracker.py
+++ server_code/Error_Tracker.py
@@ -1,34 +1,47 @@
 import anvil.server
-from anvil.tables import app_tables
+from anvil.tables import app_tables,batch_update
 import anvil.users
 from datetime import datetime
 
 @anvil.server.http_endpoint("/internal/log_error")
 def log_error():
-
+    try:
+        current_time = datetime.utcnow()
         body = anvil.server.request.body_json
         error = body['error']
-        
+        session = anvil.server.get_session_id()
         additional_data = body['additional_data']
-        additional_data['session'] = anvil.server.get_session_id()
+        additional_data['session'] = session
         error_row = app_tables.error.get(Error=error)
         user = anvil.users.get_user()
+        
         if error_row:
-            users = error_row['Users']
-            users.append(anvil.users.get_user())
-            error_row['Users'] = list(set(users))
-            error_row['User_Count'] = len(users)
-            error['Traceback'] = body['traceback']
+            if session in error_row['Sessions']:
+                return
+            if user:
+                users = error_row['Users']
+                users.append(anvil.users.get_user())
+                users = list(set(users))
+                error_row['Users'] = users
+                error_row['User_Count'] = len(users)
+            error_row['Error_Count'] += 1
+            sessions = error_row['Sessions']
+            sessions.append(session)
+            error_row['Sessions'] = sessions
+            error_row['Traceback'] = body['traceback']
             if error_row['status'] == "fixed":
                 error_row['status'] = "reappeared"
         else:
-            error_row = app_tables.error.add_row(Error=error,timeline=[],Users=[user], User_Count=1, status="pending", traceback = body['traceback'])
+            error_row = app_tables.error.add_row(Error=error,timeline=[],Users=[user], User_Count=1, status="pending", Traceback = body['traceback'], Error_Count = 1, Sessions = [session], first_appeared = current_time)
     
-        current_time = datetime.utcnow()
-        error_row['last_appeared'] = current_time
         
+        error_row['last_appeared'] = current_time
+            
         new_timeline = app_tables.timeline.add_row(datetime=current_time, type="user_error", User=user, Additional_Info=additional_data)
         
         timeline = error_row['timeline']
         timeline.append(new_timeline)
         error_row['timeline'] = timeline
+        
+    except Exception as e:
+        app_tables.error.add_row(Error = str(e))
