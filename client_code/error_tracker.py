0ca89b31006f04eed6d1349331cc41f33c79f7bc
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-03-10 14:04:47 +0000

    Edited error_tracker, settings and Error_Tracker

--- client_code/error_tracker.py
+++ client_code/error_tracker.py
@@ -1,12 +1,23 @@
-from anvil.js import window
+from anvil.js.window import UAParser, JSON, fetch
 import anvil.server
 import anvil.js
 import json
-def collect_error(err, **additional_data):
+
+
+def convert_to_py(js_object):
+    return json.loads(JSON.stringify(js_object))
+    
+def collect_error(err, collect_device_info = False, **additional_data):
     
     traceback = anvil.js.call("get_error_traceback",err)
-    
-    window.fetch(anvil.server.get_api_origin()+"/internal/log_error", {
+
+    if collect_device_info:
+        
+        ua_data = {k: ' '.join(map(str, dict(v).values())) if isinstance(v, dict) else v
+                        for k, v in convert_to_py(UAParser()).items()}
+        additional_data.update(ua_data)
+        
+    fetch(anvil.server.get_api_origin()+"/internal/log_error", {
         "method" : "POST",
         "body" : json.dumps({"error": str(err), "additional_data": additional_data, "traceback":traceback}),
         "headers": {
--- server_code/Error_Tracker.py
+++ server_code/Error_Tracker.py
@@ -13,36 +13,34 @@ def log_error():
         session = anvil.server.get_session_id()
         additional_data = body['additional_data']
         additional_data['session'] = session
-        error_row = app_tables.error.get(Error=error, Traceback = traceback)
+        
+        error_row = app_tables.error.get(error_msg = error, traceback = traceback)
         user = anvil.users.get_user()
         
         if error_row:
-            if session in error_row['Sessions']:
-                return
+            if app_tables.error.get(sessions = [session], error_msg = error, traceback = traceback):
+                return #Same error in this session already
             if user:
-                users = error_row['Users']
+                users = error_row['users']
                 users.append(anvil.users.get_user())
                 users = list(set(users))
-                error_row['Users'] = users
-                error_row['User_Count'] = len(users)
-            error_row['Error_Count'] += 1
-            sessions = error_row['Sessions']
+                error_row['users'] = users
+                error_row['user_count'] = len(users)
+            error_row['error_count'] += 1
+            sessions = error_row['sessions']
             sessions.append(session)
-            error_row['Sessions'] = sessions
-            error_row['Traceback'] = traceback
+            error_row['sessions'] = sessions
+            error_row['traceback'] = traceback
             if error_row['status'] == "fixed":
                 error_row['status'] = "reappeared"
+                app_tables.timeline.add_row(type="error_reappeared", datetime = current_time, error = error_row)
         else:
-            error_row = app_tables.error.add_row(Error=error,timeline=[],Users=[user], User_Count=1, status="pending", Traceback = body['traceback'], Error_Count = 1, Sessions = [session], first_appeared = current_time)
+            error_row = app_tables.error.add_row(error_msg = error, users=[user], user_count=1, status="pending", traceback = body['traceback'], error_count = 1, sessions = [session], first_appeared = current_time)
     
         
         error_row['last_appeared'] = current_time
             
-        new_timeline = app_tables.timeline.add_row(datetime=current_time, type="user_error", User=user, Additional_Info=additional_data)
-        
-        timeline = error_row['timeline']
-        timeline.append(new_timeline)
-        error_row['timeline'] = timeline
+        app_tables.timeline.add_row(datetime=current_time, type="user_error", user=user, additional_info = additional_data, error = error_row)
         
     except Exception as e:
-        app_tables.error.add_row(Error = str(e))
+        app_tables.error.add_row(error_msg = str(e))
