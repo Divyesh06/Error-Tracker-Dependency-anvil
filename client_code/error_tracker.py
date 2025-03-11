abcc4f151f10287c06a82ff58cc9c2be39366f73
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-03-11 09:33:42 +0000

    Edited error_tracker

--- client_code/error_tracker.py
+++ client_code/error_tracker.py
@@ -8,19 +8,22 @@ def convert_to_py(js_object):
     return json.loads(JSON.stringify(js_object))
     
 def collect_error(err, collect_device_info = False, **additional_data):
-    err_str = f"{type(err).__name__}: {str(err)}"
-    traceback = anvil.js.call("get_error_traceback",err)
-
-    if collect_device_info:
-        
-        ua_data = {k: ' '.join(map(str, dict(v).values())) if isinstance(v, dict) else v
-                        for k, v in convert_to_py(UAParser()).items()}
-        additional_data.update(ua_data)
-        
-    fetch(anvil.server.get_api_origin()+"/internal/log_error", {
-        "method" : "POST",
-        "body" : json.dumps({"error": err_str, "additional_data": additional_data, "traceback":traceback}),
-        "headers": {
-            "Content-Type": "application/json",
-        },
-    })
+    try:
+        err_str = f"{type(err).__name__}: {str(err)}"
+        traceback = anvil.js.call("get_error_traceback",err)
+    
+        if collect_device_info:
+            
+            ua_data = {k: ' '.join(map(str, dict(v).values())) if isinstance(v, dict) else v
+                            for k, v in convert_to_py(UAParser()).items()}
+            additional_data.update(ua_data)
+            
+        fetch(anvil.server.get_api_origin()+"/internal/log_error", {
+            "method" : "POST",
+            "body" : json.dumps({"error": err_str, "additional_data": additional_data, "traceback":traceback}),
+            "headers": {
+                "Content-Type": "application/json",
+            },
+        })
+    except Exception as e:
+        print(f"(Error Tracker) Failed to Report Error due to the following exception: {e}")
